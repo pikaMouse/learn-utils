e527e5c190e4e90985696e1e4f5d63fd99a30434
jiangfeng <jiangfeng@bigo.sg>
2018-12-27 20:00:46 +0800

    change:补充MemoryUtil

--- app/src/main/java/com/example/pikamouse/learn_utils/MainActivity.java
+++ app/src/main/java/com/example/pikamouse/learn_utils/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.pikamouse.learn_utils.utils.MemoryUtil;
 import com.example.pikamouse.learn_utils.utils.ProcessUtil;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTextView = findViewById(R.id.tv_text);
-        mTextView.setText("Pid: " + ProcessUtil.getCurrentPid());
+        mTextView.setText("PSS: " + MemoryUtil.getAppPSSInfo(this, ProcessUtil.getCurrentPid()).totalPSS + "\n" +
+                "allocateMem: " + MemoryUtil.getDalvikHeapInfo().allocatedMem);
     }
 }
--- app/src/main/java/com/example/pikamouse/learn_utils/utils/MemoryUtil.java
+++ app/src/main/java/com/example/pikamouse/learn_utils/utils/MemoryUtil.java
@@ -6,7 +6,10 @@ import android.os.Build;
 import android.os.Debug;
 import android.util.Log;
 
-import com.example.pikamouse.learn_utils.MyApplication;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * @author: jiangfeng
@@ -17,7 +20,7 @@ import com.example.pikamouse.learn_utils.MyApplication;
 public class MemoryUtil {
 
     private final static String TAG = "MemoryUtil";
-
+    private final static String FILE_MEM = "/proc/meminfo";
 
     /**
      * Dalvik堆内存
@@ -50,4 +53,55 @@ public class MemoryUtil {
         public long lowMemThreshold;         //内存占用满的阀值，超过即认为低内存运行状态，可能会Kill process
         public boolean isLowMem;             //是否低内存运行状态
     }
+
+    public static DalvikHeapInfo getDalvikHeapInfo() {
+        Runtime runtime = Runtime.getRuntime();
+        DalvikHeapInfo dalvikHeapInfo = new DalvikHeapInfo();
+        dalvikHeapInfo.maxMem = runtime.maxMemory() / 1024;
+        dalvikHeapInfo.freeMem = runtime.freeMemory() / 1024;
+        dalvikHeapInfo.allocatedMem = (runtime.totalMemory() - runtime.freeMemory()) / 1024;
+        return dalvikHeapInfo;
+    }
+
+    public static PSSInfo getAppPSSInfo(Context context, int pid) {
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        Debug.MemoryInfo memoryInfo = am.getProcessMemoryInfo(new int[]{pid})[0];
+        PSSInfo pssInfo = new PSSInfo();
+        pssInfo.dalvikPSS = memoryInfo.dalvikPss;
+        pssInfo.nativePSS = memoryInfo.nativePss;
+        pssInfo.otherPSS = memoryInfo.otherPss;
+        pssInfo.totalPSS = memoryInfo.getTotalPss();
+        return pssInfo;
+    }
+
+    public static RAMInfo getRAMInfo(Context context) {
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
+        am.getMemoryInfo(memoryInfo);
+        RAMInfo ramInfo = new RAMInfo();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            ramInfo.totalMem = memoryInfo.totalMem / 1024;
+        } else {
+            FileReader fr;
+            BufferedReader br;
+            try {
+                fr = new FileReader(FILE_MEM);
+                br = new BufferedReader(fr);
+                String text = br.readLine();
+                String[] array = text.split("\\s+");
+                // 单位为KB
+                ramInfo.totalMem = Long.valueOf(array[1]);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        ramInfo.availMem = memoryInfo.availMem / 1024;
+        ramInfo.isLowMem = memoryInfo.lowMemory;
+        ramInfo.lowMemThreshold = memoryInfo.threshold / 1024;
+        return ramInfo;
+    }
+
+
 }
