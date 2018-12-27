b61cfe922561319400b1bf20b177b9ce3049bf4a
jiangfeng <jiangfeng@bigo.sg>
2018-12-27 18:20:46 +0800

    change:增加ThreadUtil

---
+++ app/src/main/java/com/example/pikamouse/learn_utils/utils/ThreadUtil.java
@@ -0,0 +1,48 @@
+package com.example.pikamouse.learn_utils.utils;
+
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author: jiangfeng
+ * @date: 2018/12/27
+ */
+public class ThreadUtil {
+
+    private static final int CPU_COUNT = Runtime.getRuntime().availableProcessors();
+    private static final int CORE_POOL_SIZE = Math.max(2, Math.min(CPU_COUNT - 1, 4));
+    private static final int MAXIMUM_POOL_SIZE = CPU_COUNT * 2 + 1;
+    private static final int KEEP_ALIVE_SECONDS = 30;
+
+    private static final Executor THREAD_POOL_EXECUTOR;
+
+    private static final ThreadFactory sThreadFactory = new ThreadFactory() {
+
+        private final AtomicInteger num = new AtomicInteger(1);
+
+        @Override
+        public Thread newThread(Runnable r) {
+            return new Thread(r, "Thread" + num.getAndIncrement());
+        }
+    };
+
+    private static final BlockingQueue<Runnable> sPoolWorkQueue = new LinkedBlockingQueue<>();
+
+    static {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                CORE_POOL_SIZE, MAXIMUM_POOL_SIZE, KEEP_ALIVE_SECONDS, TimeUnit.SECONDS,
+                sPoolWorkQueue, sThreadFactory);
+        threadPoolExecutor.allowCoreThreadTimeOut(true);
+        THREAD_POOL_EXECUTOR = threadPoolExecutor;
+    }
+
+    public static void executeInExcecutor(Runnable r) {
+        THREAD_POOL_EXECUTOR.execute(r);
+    }
+}
